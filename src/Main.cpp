b05caacf085caf589a8584e0a11ee08416ae1398
Alejandro Saucedo <axsauze@gmail.com>
2020-06-28 15:10:44 +0100

    Added header files for base clases

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,18 @@
-#include <iostream>
+#include "./Constants.h"
+#include "./Game.h"
 
 int main(int argc, char *args[]) {
-    // TODO:
-    // Game *game = new Game():
+    Game *game = new Game();
 
-    std::cout << "Game is running..." << std::endl;
+    game->Initialize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    while (game->IsRunning()) {
+        game->ProcessInput();
+        game->Update();
+        game->Render();
+    }
+
+    game->Destroy();
+    
     return 0;
 }
